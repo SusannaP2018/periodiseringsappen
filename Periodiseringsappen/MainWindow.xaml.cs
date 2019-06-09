819f356728022ca089ef39474de18b5c6c25f18f
Susanna Persson <zestwerk@gmail.com>
2019-06-09 18:51:56 +0200

    Comment added.

--- Periodiseringsappen/MainWindow.xaml.cs
+++ Periodiseringsappen/MainWindow.xaml.cs
@@ -13,7 +13,7 @@ using System.Windows.Media.Imaging;
 using System.Windows.Navigation;
 using System.Windows.Shapes;
 
-namespace Periodiseringsappen
+namespace Periodiseringsappen // new app
 {
     /// <summary>
     /// Interaction logic for MainWindow.xaml
