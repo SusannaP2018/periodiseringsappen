9fe0ce21ceec7c709e9c89fea6591620f62eb270
Susanna Persson <zestwerk@gmail.com>
2019-06-09 21:59:39 +0200

    Interface in place. New beginning window added. Input starting amount function added. Save starting amount to bin file function added.

--- Periodiseringsappen/MainWindow.xaml.cs
+++ Periodiseringsappen/MainWindow.xaml.cs
@@ -1,5 +1,6 @@
 ﻿using System;
 using System.Collections.Generic;
+using System.IO;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
@@ -13,16 +14,47 @@ using System.Windows.Media.Imaging;
 using System.Windows.Navigation;
 using System.Windows.Shapes;
 
-namespace Periodiseringsappen // new app 
+namespace Periodiseringsappen
 {
     /// <summary>
     /// Interaction logic for MainWindow.xaml
     /// </summary>
     public partial class MainWindow : Window
     {
+        Amount bigSum = new Amount();
+
+        const string fileName = "amounts.bin";
         public MainWindow()
         {
             InitializeComponent();
+
+            if (File.Exists(fileName)) // Reads the file, if there is one.
+            {
+                bigSum = (Amount)FileOperations.Deserialize(fileName);
+            }
+            else
+            {
+                NewStart ns = new NewStart();
+                ns.Show();
+                this.Close();
+            }
+
+            txtTotalSum.Text = bigSum.Value.ToString();
+            
+        }
+
+        private void btnSave_Click(object sender, RoutedEventArgs e)
+        {
+            bigSum.Value = Decimal.Parse(txtTotalSum.Text);
+            FileOperations.Serialize(bigSum, fileName);
+            this.Close();
+        }
+
+        private void btnStart_Click(object sender, RoutedEventArgs e)
+        {
+            NewStart ns = new NewStart();
+            ns.Show();
+            this.Close();
         }
     }
 }
